69c8139c4710d89d9902e756c5e6f5ae5b2bc9ea
Harshika Gupta <gupta.harshik@northeastern.edu>
2021-12-01 02:57:56 -0500

    corrected

--- index.js
+++ index.js
@@ -3,52 +3,54 @@ var crypt = require('crypto');
 var ses = new aws.SES({ region: "us-east-1" });
 var DynamoDB = new aws.DynamoDB.DocumentClient();
 
-require('dotenv').config();
-
 exports.handler = (event, context, callback) => {
 
     let message = JSON.parse(event.Records[0].Sns.Message);
-    // let HASH = message.token;
 
-    // let searchParams = {
-    //     TableName: "dynamo_db",
-    //     Key: {
-    //         "token": HASH
-    //     }
-    // };
+    console.log(JSON.stringify(message));
+    
+    let username = message.username;
+    let HASH = message.token;
 
-    // console.log("Checking if record already present in DB!!");
+    let searchParams = {
+        TableName: "dynamodb-table",
+        Key: {
+            "one-time-token": HASH
+        }
+    };
+    
 
-    // DynamoDB.get(searchParams, function(error, record){
+    console.log("Checking if record already present in DB!!");
+
+    DynamoDB.get(searchParams, function(error, record){
         
-    //     if(error) {
-    //         console.log("Error in DynamoDB get method ",error);
-    //     } else {
-    //         console.log("Success in get method dynamoDB", record);
-    //         console.log(JSON.stringify(record));
-    //         let isPresent = false;
+        if(error) {
 
-    //         if (record.Item == null || record.Item == undefined) {
-    //             isPresent = false;
-    //         } else {
-    //             if(record.Item.ttl < Math.floor(Date.now() / 1000))
-    //                 isPresent = false;
-    //             else
-    //                 isPresent = true;
-    //                 sendEmail(message);
-    //         }
-    //     }
-    // })
-    sendEmail(message);
+            console.log("Error in DynamoDB get method ",error);
+
+        } else {
+
+            console.log("Success in get method dynamoDB", record);
+            console.log(JSON.stringify(record));
+            let isPresent = false;
+
+            if (record.Item == null || record.Item == undefined) {
+                isPresent = false;
+                // sendEmail(message);
+            }
+            sendEmail(message);
+        }
+    })
 };
 
 var sendEmail = (data) => {
+
     let link = `http://${data.domainName}/v1/verifyUserEmail?email=${data.username}&token=${data.token}`;
+
     let body = "Hello "+ data.first_name +",\n\n"+
     "You registered an account on our application, before being able to use your account you need to verify that this is your email address by clicking here:" +"\n\n\n"+
     "Kind Regards,"+data.username+"\n\n\n"+
     link
-
     let from = "no-reply@"+data.domainName
     let emailParams = {
         Destination: {
